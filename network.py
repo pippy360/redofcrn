0d5cad4b47f79205c2bd6890699e96a0ae6714b8
Tom Murphy <murphyt7@tcd.ie>
2017-12-17 13:37:43 +0000

    Update network.py

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ def purpleOne(name, inputs, inputSize, outputSize, isPurple):
 	with tf.variable_scope(name) as scope:
 	    #conv 1x1 /1
 		conv = inputs
-		conv = tf.layers.conv2d(conv, filters=inputSize, kernel_size=1, padding='SAME', use_bias)
+		conv = tf.layers.conv2d(conv, filters=inputSize, kernel_size=1, padding='SAME')
 		conv = tf.layers.batch_normalization(conv)
 		conv = tf.nn.relu(conv)
 
@@ -29,31 +29,41 @@ def purpleOne(name, inputs, inputSize, outputSize, isPurple):
 		return conv
 
 
-def inference(inputs):
+def inference(input_data):
 	#input 304 x 228 x 3
 
-	conv = tf.layers.conv2d(inputs, filters=64, kernel_size=7, padding='SAME')
-	conv = tf.layers.batch_normalization(conv)
-	conv = tf.nn.relu(conv)
-	conv = tf.layers.max_pooling2d(conv, pool_size=3, strides=2)
+	conv1 = tf.layers.conv2d(input_data, filters=64, kernel_size=7, padding='SAME')
+	conv2 = tf.layers.batch_normalization(conv1)
+	conv3 = tf.nn.relu(conv2)
+	conv4 = tf.layers.max_pooling2d(conv3, pool_size=3, strides=2)
 
-	conv = purpleOne("purple1_", conv, inputSize=64, outputSize=256)
+	conv5 = purpleOne("purple1_", conv4, inputSize=64, outputSize=256)
 	for i in range(2):
-		conv = purpleOne("blue1_"+str(i), conv, inputSize=64, outputSize=256, isPurple=False)
+		conv5 = purpleOne("blue1_"+str(i), conv5, inputSize=64, outputSize=256, isPurple=False)
 
-	conv = purpleOne("purple2_", conv, inputSize=128, outputSize=512)
+	conv6 = purpleOne("purple2_", conv5, inputSize=128, outputSize=512)
 	for i in range(3):
-		conv = purpleOne("blue2_"+str(i), conv, inputSize=128, outputSize=512, isPurple=False)
+		conv6 = purpleOne("blue2_"+str(i), conv6, inputSize=128, outputSize=512, isPurple=False)
 
-	conv = purpleOne("purple3_", conv, inputSize=256, outputSize=1024)
+	conv7 = purpleOne("purple3_", conv6, inputSize=256, outputSize=1024)
 	for i in range(5):
-		conv = purpleOne("blue3_"+str(i), conv, inputSize=256, outputSize=1024, isPurple=False)
+		conv7 = purpleOne("blue3_"+str(i), conv7, inputSize=256, outputSize=1024, isPurple=False)
 
-	conv = purpleOne("purple4_", conv, inputSize=512, outputSize=2048)
+	conv8 = purpleOne("purple4_", conv7, inputSize=512, outputSize=2048)
 	for i in range(2):
-		conv = purpleOne("blue4_"+str(i), conv, inputSize=512, outputSize=2048, isPurple=False)
+		conv8 = purpleOne("blue4_"+str(i), conv8, inputSize=512, outputSize=2048, isPurple=False)
 
-	return conv
+	conv9 = tf.layers.conv2d(conv8, filters=1024, kernel_size=1, padding='SAME')
+	conv10 = tf.layers.batch_normalization(conv9, name = layerName)
+
+	conv11 = up_project(conv10, kernel_size=3, filters_size=512, id=0)
+	conv12 = up_project(conv11, kernel_size=3, filters_size=256, id=0)
+	conv13 = up_project(conv12, kernel_size=3, filters_size=128, id=0)
+	conv14 = up_project(conv13, kernel_size=3, filters_size=64, id=0)
+
+	conv15 = tf.layers.conv2d(conv14, filters=1, kernel_size=3, padding='SAME')
+
+	return conv15
 
 
 ####
@@ -78,7 +88,7 @@ def interleave(tensors, axis):
 	return tf.reshape(tf.stack(tensors, axis + 1), new_shape)
 
 
-def unpool_as_conv(input_data, size, id, ReLU = False, BN = True):
+def unpool_as_conv(input_data, convOutputSize, id, ReLU = False, BN = True):
 
 	# Model upconvolutions (unpooling + convolution) as interleaving feature
 	# maps of four convolutions (A,B,C,D). Building block for up-projections. 
@@ -87,25 +97,25 @@ def unpool_as_conv(input_data, size, id, ReLU = False, BN = True):
 	# Convolution A (3x3)
 	# --------------------------------------------------
 	layerName = "layer%s_ConvA" % (id)
-    outputA = tf.layers.conv2d(input_data, filters=size[3], kernel_size=(3, 3), padding='SAME', name=layerName)
+    outputA = tf.layers.conv2d(input_data, filters=convOutputSize, kernel_size=(3, 3), padding='SAME', name=layerName)
 
 	# Convolution B (2x3)
 	# --------------------------------------------------
 	layerName = "layer%s_ConvB" % (id)
 	padded_input_B = tf.pad(input_data, [[0, 0], [1, 0], [1, 1], [0, 0]], "CONSTANT")
-    outputB = tf.layers.conv2d(padded_input_B, filters=size[3], kernel_size=(2, 3), padding='VALID', name=layerName)
+    outputB = tf.layers.conv2d(padded_input_B, filters=convOutputSize, kernel_size=(2, 3), padding='VALID', name=layerName)
 
 	# Convolution C (3x2)
 	# --------------------------------------------------
 	layerName = "layer%s_ConvC" % (id)
 	padded_input_C = tf.pad(input_data, [[0, 0], [1, 1], [1, 0], [0, 0]], "CONSTANT")
-	outputC = tf.layers.conv2d(padded_input_C, filters=size[3], kernel_size=(3, 2), padding='VALID', name=layerName)
+	outputC = tf.layers.conv2d(padded_input_C, filters=convOutputSize, kernel_size=(3, 2), padding='VALID', name=layerName)
 
 	# Convolution D (2x2)
 	# --------------------------------------------------
 	layerName = "layer%s_ConvD" % (id)
 	padded_input_D = tf.pad(input_data, [[0, 0], [1, 0], [1, 0], [0, 0]], "CONSTANT")
-	outputD = tf.layers.conv2d(padded_input_D, filters=size[3], kernel_size=(2, 2), padding='VALID', name=layerName)
+	outputD = tf.layers.conv2d(padded_input_D, filters=convOutputSize, kernel_size=(2, 2), padding='VALID', name=layerName)
 
 	# Interleaving elements of the four feature maps
 	# --------------------------------------------------
@@ -123,32 +133,28 @@ def unpool_as_conv(input_data, size, id, ReLU = False, BN = True):
 	return result
 
 
-def up_project(inputs, size, id, stride = 1):
+def up_project(input_data, kernel_size, filters_size, id):
 
 	# Create residual upsampling layer (UpProjection)
 
-	inputs = self.get_output()
-
 	# Branch 1
 	# Interleaving Convs of 1st branch
-	out = unpool_as_conv(inputs, size, stride)
-	ReLU=True, 
-	BN=True
-
+	id_br1 = "%s_br1" % (id)
+	branch1_out1 = unpool_as_conv(input_data, convOutputSize=filters_size, ReLU=True, BN=True)
 
 	# Convolution following the upProjection on the 1st branch
-	self.conv(size[0], size[1], size[3], stride, stride, relu = False)
+	branch1_out2 = tf.layers.conv2d(branch1_out1, filters=filters_size, kernel_size=kernel_size, padding='SAME')
 
 	layerName = "layer%s_BN" % (id)
-	self.batch_normalization(name = layerName, scale_offset=True, relu = False)
+	branch1_out3 = tf.layers.batch_normalization(branch1_out2, name = layerName)
 
 	# Output of 1st branch
-	branch1_output = self.get_output()
+	branch1_output = branch1_out3
 
 	# Branch 2
 	# Interleaving convolutions and output of 2nd branch
-	branch2_output = self.unpool_as_conv(inputs, size, stride, ReLU=False)
-	BN=True
+	id_br2 = "%s_br2" % (id)
+	branch2_output = unpool_as_conv(input_data, convOutputSize=filters_size, ReLU=False, BN=True)
 
 	# sum branches
 	layerName = "layer%s_Sum" % (id)
@@ -157,5 +163,4 @@ def up_project(inputs, size, id, stride = 1):
 	layerName = "layer%s_ReLU" % (id)
 	output = tf.nn.relu(output, name=layerName)
 
-	self.feed(output)
-	return self
+	return out

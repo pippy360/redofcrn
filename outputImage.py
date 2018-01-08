f64a2bdd1db0b06d93ae689ffd3271b5371839d4
Tom Murphy <murphyt7@tcd.ie>
2018-01-08 19:37:27 +0000

    Create outputImage.py

---
+++ outputImage.py
@@ -0,0 +1,129 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import math
+import time
+
+import numpy as np
+import tensorflow as tf
+
+import network
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--eval_dir', type=str, default='./eval',
+                    help='Directory where to write event logs.')
+
+parser.add_argument('--eval_data', type=str, default='test.csv',
+                    help='Either `test` or `train_eval`.')
+
+parser.add_argument('--checkpoint_dir', type=str, default='./train_checkpoint',
+                    help='Directory where to read model checkpoints.')
+
+parser.add_argument('--eval_interval_secs', type=int, default=120,
+                    help='How often to run the eval.')
+
+parser.add_argument('--num_examples', type=int, default=1000,
+                    help='Number of examples to run.')
+
+parser.add_argument('--run_once', type=bool, default=False,
+                    help='Whether to run eval only once.')
+
+batch_size = 1
+
+IMAGE_HEIGHT = 228
+IMAGE_WIDTH = 304
+TARGET_HEIGHT = 128
+TARGET_WIDTH = 160
+BATCH_SIZE = 8
+
+def eval_once(saver, summary_writer, logits, summary_op):
+  """Run Eval once.
+  Args:
+    saver: Saver.
+    summary_writer: Summary writer.
+    top_k_op: Top K op.
+    summary_op: Summary op.
+  """
+  with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+      # Restores from checkpoint
+      saver.restore(sess, ckpt.model_checkpoint_path)
+      # Assuming model_checkpoint_path looks something like:
+      #   /my-favorite-path/cifar10_train/model.ckpt-0,
+      # extract global_step from it.
+      global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+    else:
+      print('No checkpoint file found')
+      return
+
+    # Start the queue runners.
+    coord = tf.train.Coordinator()
+    try:
+      threads = []
+      for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+        threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
+                                         start=True))
+
+      return_values = sess.run(logits)
+      pred = return_values[0]
+      formatted = ((pred[0,:,:,0]) * 255 / np.max(pred[0,:,:,0])).astype('uint8')
+      img = Image.fromarray(formatted)
+      img.save("./output.jpg")
+      #save it....
+      summary = tf.Summary()
+      summary.ParseFromString(sess.run(summary_op))
+      summary_writer.add_summary(summary, global_step)
+    except Exception as e:  # pylint: disable=broad-except
+      coord.request_stop(e)
+
+    coord.request_stop()
+    coord.join(threads, stop_grace_period_secs=10)
+
+
+
+def evaluate():
+  """Eval CIFAR-10 for a number of steps."""
+  with tf.Graph().as_default() as g:
+    # Get images and labels for CIFAR-10.
+    original_size = (480, 640)
+    
+    imageSize = (IMAGE_HEIGHT, IMAGE_WIDTH)
+    depthImageSize = (TARGET_HEIGHT, TARGET_WIDTH)
+
+    image_filename_input = tf.read_file(FLAGS.eval_dir)
+    image = tf.image.decode_jpg(image_filename_input, channels=1)
+    images = tf.train.batch(
+            [image],
+            batch_size=1,
+            num_threads=1,
+            capacity= 50 + 3 * 1,
+    )
+
+    logits = network.inference(images)
+
+    # Restore the moving average version of the learned variables for eval.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        network.MOVING_AVERAGE_DECAY)
+    variables_to_restore = variable_averages.variables_to_restore()
+    saver = tf.train.Saver(variables_to_restore)
+
+    # Build the summary operation based on the TF collection of Summaries.
+    summary_op = tf.summary.merge_all()
+
+    summary_writer = tf.summary.FileWriter(FLAGS.eval_dir, g)
+
+    eval_once(saver, summary_writer, [logits], summary_op)
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+  evaluate()
+
+
+if __name__ == '__main__':
+  FLAGS = parser.parse_args()
+tf.app.run()
